e5f7c5e86705dc6292d35b20fb01e8913b2b64c7
Keshav Gupta <guptakeshav1235@gmail.com>
2024-10-28 14:12:58 +0530

    Initial setup with frontend and backend folders

---
+++ backend/twitter.api/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace twitter.api.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet]
+        [Route("signup")]
+        public async Task<IActionResult> signup()
+        {
+            return Ok("You hit the signup endpoint");
+        }
+    }
+}
